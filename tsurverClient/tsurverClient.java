4bff94000055835bedac2f9ac4033a85c14cc1b9
tatsukibebra <103999800+tatsukibebra@users.noreply.github.com>
2023-05-10 08:25:18 +0300

    Update tsurverClient.java

--- tsurverClient/tsurverClient.java
+++ tsurverClient/tsurverClient.java
@@ -1,1 +1,37 @@
+import java.io.*;
+import java.net.*;
 
+class TsurverClient {
+
+    private final BufferedReader reader;
+    private final BufferedWriter writer;
+    Tsurver(Socket ssocket) {
+        try {
+            this.socket = server.accept();
+            this.reader = createReader();
+            this.writer = createWriter();
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void writeLine(String message) {
+        try {
+            writer.write(message);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private BufferedReader createReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+    private BufferedWriter createWriter() throws IOException {
+        return new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    }
+    public void close () throws IOException {
+        socket.close();
+        reader.close();
+        writer.close();
+    }
+}
