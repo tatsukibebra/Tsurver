3b321dd10c4bce253de169fec777b980b3bde6d2
tatsukibebra <103999800+tatsukibebra@users.noreply.github.com>
2023-05-10 13:16:16 +0300

    Update Main.java

--- tsurverClient/Main.java
+++ tsurverClient/Main.java
@@ -3,10 +3,10 @@ import java.net.ServerSocket;
 public class Main {
     public static void main(String[] args) {
         try {
-            ServerSocket server = new          ServerSocket("there ip", 60606);
+            ServerSocket server = new          ServerSocket("<<СЮДА АЙПИШНИК>>", 60606);
             System.out.println("Server started!");
             TsurverClient tsurverClient = new Tsurver(socket);
-            tsurver.writeLine("Hello from Tsurver!");
+            TsurverClienr.writeLine("Hello from Tsurver!");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
