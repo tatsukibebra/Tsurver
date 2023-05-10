e8e4b158760ff308d982044aabaea61c4756a5c4
tatsukibebra <103999800+tatsukibebra@users.noreply.github.com>
2023-05-10 17:54:07 +0300

    Update Main.java

--- tsurverClient/Main.java
+++ tsurverClient/Main.java
@@ -1,7 +1,8 @@
 import java.net.Socket;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
