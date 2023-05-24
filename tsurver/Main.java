7da895f70e5db642eabfea07e9df9d18d7f99879
tatsukibebra <103999800+tatsukibebra@users.noreply.github.com>
2023-05-24 11:22:41 +0300

    Update Main.java

--- tsurver/Main.java
+++ tsurver/Main.java
@@ -6,7 +6,8 @@ public class Main {
             ServerSocket server = new ServerSocket(60606);
             System.out.println("Server started!");
             Tsurver tsurver = new Tsurver(server);
-            tsurver.writeLine("Hello from Tsurver!");
+            Tsurver.writeLine("Hello from Tsurver!");
+            
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
