a8aa68c29eec82dff99b98006e8f21c0a726a801
tatsukibebra <103999800+tatsukibebra@users.noreply.github.com>
2023-06-02 12:40:55 +0300

    Update Main.java

--- tsurverClient/Main.java
+++ tsurverClient/Main.java
@@ -1,21 +1,19 @@
 import java.net.Socket;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try (Socket server = socket("<<IP SERVER>>", 60606)) {
+        try (Socket server = socket("<<<IP SERVER>>>", 60606)) {
             TsurverClient tsurverClient = new TsurverClient(socket);
             System.out.println("Client started!");
-            String username = "tatsukibebra";
+            String username = "<<<USERNAME>>>";
             System.out.println(tsurverClient.reader.readLine());
-            TsurverClienr.writLine(username);
+            tsurverClient.writeLine(username);
             Scanner scnr = new Scanner(System.in);
             while(true) {
-                TsurverClient.writeLine(scnr.nextLine());
+                tsurverClient.writeLine(scnr.nextLine());
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
